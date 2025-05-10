440d7f44a92ee90ff1f53cf9c3594272e0bf099a
sifayet007 <sifayet2004@gmail.comm>
2025-05-10 11:31:00 +0600

    remove toggle dark and light button

--- src/components/BennarBook/BennarBook.jsx
+++ src/components/BennarBook/BennarBook.jsx
@@ -1,29 +1,8 @@
-import React, { useState } from "react";
 import bennarBook from "../../assets/pngwing 1.png";
 const BennarBook = () => {
-  const [theme, setTheme] = useState("");
-  console.log(theme);
-
-  const handleTheme = (st) => {
-    setTheme(st);
-  };
   return (
     <div>
       <main className="mt-12">
-        <div className="flex items-center gap-5">
-          <button
-            className={`${theme === "light" ? "bg-green-400" : "bg-white"}`}
-            onClick={() => handleTheme("light")}
-          >
-            light
-          </button>
-          <button
-            className={`${theme === "dark" ? "bg-green-500" : "bg-white"}`}
-            onClick={() => handleTheme("dark")}
-          >
-            Dark
-          </button>
-        </div>
         <section className="md:flex items-center p-5 md:px-[120px] md:py-20 bg-[#F3F3F3] rounded-3xl justify-between">
           <div className="">
             <h1 className="md:text-7xl/snug text-3xl font-semibold tracking-wider font-serif  line-clamp-2 ">
